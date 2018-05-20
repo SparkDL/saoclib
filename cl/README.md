f5c8de3ef84cb635ea8721e221c85fbe1399034b
pzque <zhangpc123@outlook.com>
2018-05-20 14:56:38 +0800

    optimize project structure

--- cl/README.md
+++ cl/README.md
@@ -1,2 +1,24 @@
 # cl
-All *.cl device files are here.
+All .cl deivce files are here.
+
+## 1. Setting  user environment variables
+a. Copy the 'opencl-intel-fpga-env.sh' to '/etc/profile.d'
+```bash
+sudo cp opencl-intel-fpga-env.sh /etc/profile.d 
+``` 
+b. Open the file, modify the variable 'ALTERAOCLSDKROOT' to the path where your Intel OpenCL FPGA SDK installed,
+and modify the variable 'QUARTUS_ROOTDIR_OVERRIDE' to the path where your Quartus installed.
+
+c. Restart your system or run follow command to activate these variables:
+```bash
+source /etc/profile
+```
+
+## 2. Link essential dynamic libraries to '/usr/lib'
+Run follow commands:
+```bash
+sudo ln -s $INTELFPGAOCLSDKROOT/host/linux64/lib/* /usr/lib
+sudo ln -s $AOCL_BOARD_PACKAGE_ROOT/linux64/lib/* /usr/lib
+sudo ldconfig
+```
+
---
+++ cl/build.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+hstr="""
+Usage:\n
+\tbuild.sh\t\t\t: build all .cl files \n
+\tbuild.sh all\t\t\t: build all .cl files \n
+\tbuild.sh file\t\t\t: build specific target file \n
+\tbuild.sh [file]+\t\t\t: build multiple files \n
+e.g. \n
+\tbuild \n
+\tbuild vector_add.cl \n
+\tbuild vector_add.cl matrix_mult.cl
+"""
+function echohelp() {
+	echo -e $hstr
+}
+function echogreen (){
+	echo -e "\033[0;32;1m$1\033[0m"
+}
+function echored (){
+	echo -e "\033[0;31;1m$1\033[0m"
+}
+
+function buildone() {
+	file=$1
+	echo "building $file.."
+	if [ -e $1 ]; then
+		name=${file%.*}
+		echogreen "aoc -march=emulator ./$file -o ../bin/$name.aocx"
+		aoc -march=emulator ./$file -o ../bin/$name.aocx
+	else
+		echored "file $file doesn't exist"
+	fi
+}
+
+function buildall() {
+	for file in `ls *.cl`; do
+		buildone $file
+	done
+}
+
+# no parameters or "all"
+if [ x$1 == x ]; then
+	set $1 all
+fi
+
+case $1 in
+	all)
+	       	echo "Build all targets:"
+		buildall
+	;;
+	-h) 
+		echohelp
+        ;;
+	--help)
+	       	echohelp
+	;;
+	help) 
+		echohelp
+	;;
+	*)
+		for i in "$*"; do
+			buildone $i
+		done
+	;;
+esac
+
+
